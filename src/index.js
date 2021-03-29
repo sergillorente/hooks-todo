2afd36b4d59e76728766b3eabac48e9090768d75
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-03-29 16:12:16 +0200

    Adding the structure of Redux for useReduce Hooks. Toggling with the todo tasks to update their status from uncomplete to complete and the other way around

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-export const UserContext = React.createContext()
+import TodosContext from './context'
+import todosReducer from './reducer'
+import TodoList from './components/TodoList'
 
-const username = "Sergi"
+const App = () => {
+  const initialState = useContext(TodosContext)
+  const [state, dispatch] = useReducer(todosReducer, initialState)
+
+  return (
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <TodoList />
+    </TodosContext.Provider>
+  )
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserContext.Provider value={username}>
       <App />
-    </UserContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
